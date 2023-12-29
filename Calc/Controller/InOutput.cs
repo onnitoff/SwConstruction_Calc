b4ad474e98ff91b7c91420594e0fa137e54c6316
onnitoff <onnitoff@gmail.com>
2023-12-29 17:18:20 +0300

    add input output

---
+++ Calc/Controller/InOutput.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Calc.Controller
+{
+    class InOutput
+    {
+        public InOutput() { }
+
+        public string Read()
+        {
+            string filePath = "some.txt";
+            try
+            {
+                string content = File.ReadAllText(filePath);
+                return content;
+            }
+            catch (Exception ex)
+            {
+                return "reading error\n" + ex.Message;
+                
+            }
+            
+        }
+
+        public string Write(string content)
+        {
+            string filePath = "some.txt";
+            try
+            {
+                File.WriteAllText(filePath, content);
+                return "Write was a success";
+            }
+            catch (Exception ex)
+            {
+                return "write error\n" + ex.Message;
+
+            }
+        }
+    }
+}
